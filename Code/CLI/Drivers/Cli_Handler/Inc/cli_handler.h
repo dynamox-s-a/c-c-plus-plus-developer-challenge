7a8e92e7cb36cada2acd10b9c0c0f4799a97312f
Emerson-Isaias <emerson.isaias@hotmail.com>
2025-09-21 14:26:03 -0300

    feat: Create CLI Handler
    Start the software cli handler.

---
+++ Code/CLI/Drivers/Cli_Handler/Inc/cli_handler.h
@@ -0,0 +1,55 @@
+/**
+ * @file    cli_handler.h
+ * @brief   TODO
+ *
+ * @details TODO
+ *
+ * @author  Emerson Isaias da Silva
+ * @date    21-09-2025
+ */
+
+#ifndef CLI_DRIVERS_CLI_HANDLER_H /* Include guard */
+#define CLI_DRIVERS_CLI_HANDLER_H
+
+#ifdef __cplusplus  /* C++ guard */
+extern "C" {
+#endif
+
+/*
+ *   INCLUDES
+ */
+
+/*
+ *   DEFINES
+ */
+
+/*
+ *   MACROS
+ */
+
+/*
+ *   ENUMS
+ */
+
+/*
+ *   STRUCTS
+ */
+
+/*
+ *   UNIONS
+ */
+
+/*
+ *   GLOBAL FUNCTIONS
+ */
+
+
+void Cli_Handler_Init(void);
+void Cli_Handler_Check(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Code/CLI/Drivers/Cli_Handler/Src/cli_handler.c
@@ -0,0 +1,48 @@
+/**
+ * @file    cli_handler.h
+ * @brief   TODO
+ *
+ * @details TODO
+ *
+ * @author  Emerson Isaias da Silva
+ * @date    21-09-2025
+ */
+
+/*
+ *   INCLUDES
+ */
+#include "cli_handler.h"
+ 
+/*
+ *   MACROS
+ */
+
+/*
+ *   EXTERNAL VARIABLES
+ */
+
+/*
+ *   LOCAL FUNCTIONS
+ */
+
+/**
+ * @brief TODO
+ * 
+ * @param TODO
+ * @return TODO
+ */
+void Cli_Handler_Init(void){
+
+
+}
+
+/**
+ * @brief TODO
+ * 
+ * @param TODO
+ * @return TODO
+ */
+void Cli_Handler_Check(void){
+
+
+}
