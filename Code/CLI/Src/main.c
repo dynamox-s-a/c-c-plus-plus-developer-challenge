6945151b4e04805bc31ce6a87ad0ba8c00d591d8
Emerson-Isaias <emerson.isaias@hotmail.com>
2025-09-22 21:14:10 -0300

    docs: Documentate main.c
    Documentate process in main.c

--- Code/CLI/Src/main.c
+++ Code/CLI/Src/main.c
@@ -16,6 +16,11 @@
 #include "string.h"
 
 /*
+ *   DEFINES
+ */
+#define MAIN_STREAM_SIZE        100
+
+/*
  *   MACROS
  */
 
@@ -39,25 +44,32 @@
  */
 int main(void) {
 
-    char stream[100];
+    char stream_in[MAIN_STREAM_SIZE];   /* Stream where send commands to CLI */
+    char stream_out[MAIN_STREAM_SIZE];  /* Stream where receive answere from CLI */
 
-    cli_handler_t cli_handler;
+    cli_handler_t cli_handler;          /* CLI handler */
     cli_handler_callback_t callback;
 
-    cli_handler_config_t config = {
-        .logs = true,
-        .multithread = false,
-        .stream = stream
+    cli_handler_config_t config = {     /* Configuration of CLI */
+        .logs = true,                   /* Flag to use stream tx for logs */
+        .multithread = false,           /* Flag to use multithread */
+        .decimal = 5,                   /* Decimal of ANS when answer a command */
+        .stream_rx = stream_in,         /* Set stream where send commands to CLI */
+        .stream_tx = stream_out         /* Ser stream where receive answere from CLI */
     };
     
-    Cli_Handler_Init(&cli_handler, &callback, &config, NULL);
+    Cli_Handler_Init(&cli_handler, &callback, &config, NULL);   /* Initialize handler CLI */
 
-    while(1){
+    while(1){   /* Infinit loop */
 
-        if (fgets(stream, sizeof(stream), stdin) != NULL) {
+        if (fgets(stream_in, sizeof(stream_in), stdin) != NULL) { /* Get terminal commands */
 
-            stream[strcspn(stream, "\n")] = 0; /* Remove \n of stream and set as 0 */
-            Cli_Handler_Check(&cli_handler);
+            stream_in[strcspn(stream_in, "\n")] = 0;    /* Remove \n of stream and set as 0 */
+
+            if(strlen(stream_in) > 0){                  /* Verify CLI case have a new command */
+
+                Cli_Handler_Check(&cli_handler);        /* Verify handler CLI */
+            }
         }
     }
 
