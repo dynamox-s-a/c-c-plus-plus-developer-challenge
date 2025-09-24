c49b4fa52bfe8038ec6f910ddc0046bbcc09ff1d
Emerson-Isaias <emerson.isaias@hotmail.com>
2025-09-24 00:24:56 -0300

    fix: Update names file
    Update name file of arithmetic root n

--- Code/CLI/Drivers/Arithmetic/Src/arithmetic_root_n.c
+++ Code/CLI/Drivers/Arithmetic/Src/arithmetic_root_n.c
@@ -11,6 +11,9 @@
 /*
  *   INCLUDES
  */
+#include "arithmetic_root_n.h"
+
+#include "cli_handler.h"
 
 /*
  *   MACROS
@@ -27,6 +30,11 @@
 /*
  *   CONSTANTS
  */
+const cli_handler_commands_t Arithmetic_Root_N_Cte = {
+        .name = "Root_N",
+        .description = "Root N the value to ANS - Sintax \"Root_N [0x00000000]\"\n",
+        .func_ptr = Arithmetic_Root_N
+};
 
 /**
  * @brief TODO
@@ -35,7 +43,7 @@
  * @param token Pointer of token receive
  * @return TODO
  */
-static cli_handler_error_e command_root_n(cli_handler_t* cli_handler, char* saveptr){
+static cli_handler_error_e Arithmetic_Root_N(cli_handler_t* cli_handler, char* saveptr){
     
     printf("command_root_n");
     return CLI_HANDLER_ERROR_OK; /* Return OK */
