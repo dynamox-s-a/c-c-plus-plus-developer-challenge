91c8c5a3817e81e9ed87fd7ee97f19af6e6b6e25
Emerson-Isaias <emerson.isaias@hotmail.com>
2025-09-26 00:58:29 -0300

    refact: Change main.c
    Refactory main for use controller.

--- Code/CLI/Src/main.c
+++ Code/CLI/Src/main.c
@@ -11,10 +11,7 @@
 /*
  *   INCLUDES
  */
-#include "cli_handler.h"
-
-#include "logs_file.h"
-#include "misc.h"
+#include "controller.h"
 
 #include "stdio.h"
 #include "string.h"
@@ -23,7 +20,6 @@
 /*
  *   DEFINES
  */
-#define MAIN_STREAM_SIZE        4096
 
 /*
  *   MACROS
@@ -50,63 +46,16 @@ void receive_terminal_command(cli_handler_t* cli_handler, char* stream_in, size_
  */
 int main(void) {
 
-    logs_file_handler_t logs_file_handler;  /* Log file handler */
-    cli_handler_t cli_handler;              /* CLI handler */
+    controller_handler_t controller;    /* Controller handler */
 
-    char stream_in[MAIN_STREAM_SIZE];   /* Stream where send commands to CLI */
-    char stream_out[MAIN_STREAM_SIZE];  /* Stream where receive answere from CLI */
-
-    memset(stream_in, '\0', MAIN_STREAM_SIZE);  /* Clean Stream rx CLI */
-    memset(stream_out, '\0', MAIN_STREAM_SIZE); /* Clean Stream tx CLI */
-
-    cli_handler_config_t config_cli = {     /* Configuration of CLI */
-        .multithread = false,           /* Flag to use multithread */
-        .decimal = 5,                   /* Decimal of ANS when answer a command */
-        .stream_rx = stream_in,         /* Set stream where send commands to CLI */
-        .stream_tx = stream_out         /* Ser stream where receive answere from CLI */
-
-        /* Mutex */
-    };
-
-    logs_file_handler_config_t config_logs = {
-
-        .file_name = "log.txt",         /* File name to save logs */
-        .stream = stream_out,           /* Stream where receive logs */
-        .terminal = false                 /* Flag to print log in terminal */
-    };
-
-    Cli_Handler_Init(&cli_handler, &config_cli, NULL);        /* Initialize handler CLI */
-    Logs_File_Handler_Init(&logs_file_handler, &config_logs); /* Initialize handler log file */
+    Controller_Init(&controller);       /* Initialize controller */
 
     while(1){   /* Infinit loop */
 
-        receive_terminal_command(&cli_handler, stream_in, sizeof(stream_in)); /* Receive command from terminal */
-        Logs_File_Handler_Check(&logs_file_handler); /* Check if have a new log to save */
+        Controller_Check(&controller);  /* Check controller */
     }
-
-    Logs_File_Handler_Close(&logs_file_handler); /* Close log file handler */
 
     return 0;
-}
-
-/**
- * @brief This function will check if receive a correct command and process
- * 
- * @param cli_handler Pointer of handler CLI
- * @param stream_in Stream rx data
- * @param size_stream Sizeof stream
- */
-void receive_terminal_command(cli_handler_t* cli_handler, char* stream_in, size_t size_stream){
-
-    if (fgets(stream_in, size_stream, stdin) != NULL) { /* Get terminal commands */
-
-        stream_in[strcspn(stream_in, "\n")] = 0;        /* Remove \n of stream and set as 0 */
-
-        if(strlen(stream_in) > 0){                      /* Verify CLI case have a new command */
-
-            Cli_Handler_Check(cli_handler);             /* Verify handler CLI */
-        }
-    }
 }
 
 /**
